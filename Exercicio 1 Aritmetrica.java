b3f5ce9c2ef1bbc95f8ff60f60c1ca8a91a905f9
marcoss99v <marcosss2305@gmail.com>
2025-03-31 21:55:44 -0300

    Exercicio 1 Aritmetrica.java

--- Exercicio 1 Aritmetrica.java
+++ Exercicio 1 Aritmetrica.java
@@ -12,11 +12,9 @@ public class Main {
         double maior = Math.max(num1, Math.max(num2, num3));
         double menor = Math.min(num1, Math.min(num2, num3));
         double media = (num1 + num2 + num3) / 3;
-
         System.out.println("O maior número é: " + maior);
         System.out.println("O menor número é: " + menor);
-        System.out.printf("A média é: %.2f%n", media);
-        
+        System.out.printf("A média é: %.2f%n", media);    
         scanner.close();
     }
 }
